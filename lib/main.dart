869459aecb40d397bb1f30309477d91a46a133e8
q <mboh.dong@gmail.com>
2021-11-23 21:27:07 +0700

    membuat struktur projek

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:capastone/register_page.dart';
 import 'package:flutter/material.dart';
+import 'modules/auth/screens/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RegisterPage(),
+      home: const LoginPage(),
     );
   }
 }
