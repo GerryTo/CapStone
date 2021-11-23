a5877c8fa8a8fb09b0172b35e928861cfac5c7b4
q <mboh.dong@gmail.com>
2021-11-23 21:11:50 +0700

    merapikan login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,21 +11,22 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void initState() {
+    super.initState();
     _passwordVisible = false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff0B3D66),
-        body: SingleChildScrollView(
-          child: SafeArea(
-              child: Column(
+      backgroundColor: const Color(0xff0B3D66),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
             children: [
               SizedBox(height: 70),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+                children: const [
                   Text('Gazebo',
                       style: TextStyle(
                         fontSize: 96,
@@ -34,36 +35,36 @@ class _LoginPageState extends State<LoginPage> {
                       )),
                 ],
               ),
-              SizedBox(height: 40),
+              const SizedBox(height: 40),
               Column(
                 children: [
-                  Container(
+                  SizedBox(
                     width: MediaQuery.of(context).size.width - 100,
-                    child: Text('Email',
+                    child: const Text('Email',
                         style: TextStyle(
                           fontSize: 14,
                           color: Colors.white,
                         )),
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   emailBar(context),
-                  SizedBox(height: 10),
-                  Container(
+                  const SizedBox(height: 10),
+                  SizedBox(
                     width: MediaQuery.of(context).size.width - 100,
-                    child: Text('Kata Sandi',
+                    child: const Text('Kata Sandi',
                         style: TextStyle(
                           fontSize: 14,
                           color: Colors.white,
                         )),
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   passwordBar(context),
-                  SizedBox(height: 40),
+                  const SizedBox(height: 40),
                   SizedBox(
                     width: MediaQuery.of(context).size.width - 100,
                     child: ElevatedButton(
                       onPressed: () {},
-                      child: Text('Masuk',
+                      child: const Text('Masuk',
                           style: TextStyle(
                             fontSize: 16,
                             color: Colors.black,
@@ -71,10 +72,10 @@ class _LoginPageState extends State<LoginPage> {
                       style: ElevatedButton.styleFrom(primary: Colors.white),
                     ),
                   ),
-                  SizedBox(height: 40),
+                  const SizedBox(height: 40),
                   TextButton(
                     onPressed: () {},
-                    child: Text(
+                    child: const Text(
                       'Belum punya akun? Daftar disini',
                       style: TextStyle(color: Colors.white),
                     ),
@@ -82,81 +83,79 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
             ],
-          )),
-        ));
-  }
-
-  Widget emailBar(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width - 100,
-            decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: TextField(
-                cursorColor: Colors.black,
-                style: TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  hintText: 'Email@email.com',
-                  hintStyle: TextStyle(
-                      color: Colors.grey, fontStyle: FontStyle.italic),
-                  border: InputBorder.none,
-                ),
-                onSubmitted: (value) {},
-              ),
-            ),
           ),
-        ],
+        ),
       ),
     );
   }
 
-  Widget passwordBar(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width - 100,
-            decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: TextField(
-                obscureText: !_passwordVisible,
-                obscuringCharacter: "*",
-                cursorColor: Colors.black,
-                style: TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                    hintText: 'Masukkan kata sandi',
-                    hintStyle: TextStyle(
-                        color: Colors.grey, fontStyle: FontStyle.italic),
-                    border: InputBorder.none,
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _passwordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off,
-                        color: Colors.black,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _passwordVisible = !_passwordVisible;
-                        });
-                      },
-                    )),
-                onSubmitted: (value) {},
+  Widget emailBar(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width - 100,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: TextField(
+              cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
+              decoration: const InputDecoration(
+                hintText: 'Email@email.com',
+                hintStyle:
+                    TextStyle(color: Colors.grey, fontStyle: FontStyle.italic),
+                border: InputBorder.none,
               ),
+              onSubmitted: (value) {},
             ),
           ),
-        ],
-      ),
+        ),
+      ],
+    );
+  }
+
+  Widget passwordBar(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width - 100,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: TextField(
+              obscureText: !_passwordVisible,
+              obscuringCharacter: "*",
+              cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                  hintText: 'Masukkan kata sandi',
+                  hintStyle: const TextStyle(
+                      color: Colors.grey, fontStyle: FontStyle.italic),
+                  border: InputBorder.none,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  )),
+              onSubmitted: (value) {},
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
