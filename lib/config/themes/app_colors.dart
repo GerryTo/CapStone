b92a42d0edadb5129f696e416e7b0c4e40549c02
q <mboh.dong@gmail.com>
2021-11-25 21:22:37 +0700

    Update app_colors.dart

--- lib/config/themes/app_colors.dart
+++ lib/config/themes/app_colors.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const _primaryColors = {
-    50: Color.fromRGBO(11, 61, 102, .1),
-    100: Color.fromRGBO(11, 61, 102, .2),
-    200: Color.fromRGBO(11, 61, 102, .3),
-    300: Color.fromRGBO(11, 61, 102, .4),
-    400: Color.fromRGBO(11, 61, 102, .5),
-    500: Color.fromRGBO(11, 61, 102, .6),
-    600: Color.fromRGBO(11, 61, 102, .7),
-    700: Color.fromRGBO(11, 61, 102, .8),
-    800: Color.fromRGBO(11, 61, 102, .9),
-    900: Color.fromRGBO(11, 61, 102, 1),
+    50: Color.fromRGBO(27, 150, 250, 1),
+    100: Color.fromRGBO(25, 135, 225, 1),
+    200: Color.fromRGBO(22, 120, 200, 1),
+    300: Color.fromRGBO(19, 105, 175, 1),
+    400: Color.fromRGBO(16, 90, 150, 1),
+    500: Color.fromRGBO(14, 75, 125, 1),
+    600: Color.fromRGBO(11, 61, 102, 1),
+    700: Color.fromRGBO(8, 45, 75, 1),
+    800: Color.fromRGBO(5, 30, 50, 1),
+    900: Color.fromRGBO(3, 15, 25, 1),
   };
   static const primaryColor = MaterialColor(0xff0B3D66, _primaryColors);
 }
