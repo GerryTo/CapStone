2ed6278094d53d59300fedcf3c78d85e4b22a771
q <mboh.dong@gmail.com>
2021-12-06 15:01:35 +0700

    add timestamp to feed.dart

--- lib/modules/feeds/model/feed.dart
+++ lib/modules/feeds/model/feed.dart
@@ -7,20 +7,28 @@ class Feed {
   List<String>? images;
   String? title;
   DocumentReference? userReference;
+  Timestamp? timestamp;
 
-  Feed({this.description, this.images, this.title, this.userReference});
+  Feed(
+      {this.description,
+      this.images,
+      this.title,
+      this.userReference,
+      this.timestamp});
 
   factory Feed.fromMap(Map<String, dynamic> data) => Feed(
         description: data['description'] as String?,
         images: (data['images'] as List).map((e) => e.toString()).toList(),
         title: data['title'] as String?,
         userReference: data['userReference'] as DocumentReference?,
+        timestamp: data['timestamp'] as Timestamp?,
       );
 
   Map<String, dynamic> toMap() => {
         'description': description,
         'images': images,
         'title': title,
-        'userReference': userReference
+        'userReference': userReference,
+        'timestamp': timestamp,
       };
 }
