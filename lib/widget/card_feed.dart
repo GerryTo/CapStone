ab45ab7bd088fa23cb4a59ce726b64b44dfa1b07
q <mboh.dong@gmail.com>
2021-12-06 13:41:20 +0700

    move UserAvatar to its own class

--- lib/widget/card_feed.dart
+++ lib/widget/card_feed.dart
@@ -1,5 +1,6 @@
 import 'package:capstone/modules/feeds/model/feed.dart';
 import 'package:capstone/routes/routes.dart';
+import 'package:capstone/widget/user_avatar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -49,53 +50,6 @@ class CardFeed extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class UserAvatar extends StatelessWidget {
-  const UserAvatar(
-    this.userRef, {
-    Key? key,
-  }) : super(key: key);
-
-  final DocumentReference? userRef;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<DocumentSnapshot>(
-      future: userRef?.get(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return _error();
-        }
-
-        if (snapshot.hasData && !snapshot.data!.exists) {
-          return noData();
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
-          return CircleAvatar(
-              foregroundImage: NetworkImage(data["avatar_url"]));
-        }
-        return noData();
-      },
-    );
-  }
-
-  CircleAvatar _error() {
-    return const CircleAvatar(
-      foregroundImage:
-          NetworkImage('https://via.placeholder.com/64x64?text=Error'),
-    );
-  }
-
-  CircleAvatar noData() {
-    return const CircleAvatar(
-      foregroundImage:
-          NetworkImage('https://via.placeholder.com/64x64?text=No+Data'),
     );
   }
 }
