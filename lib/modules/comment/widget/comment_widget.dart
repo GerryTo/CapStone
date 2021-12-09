f67b3cc43d6078029eade2e1f769da132b7d392e
q <mboh.dong@gmail.com>
2021-12-09 18:57:42 +0700

    menampilkan info pemberi comment

--- lib/modules/comment/widget/comment_widget.dart
+++ lib/modules/comment/widget/comment_widget.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:capstone/modules/auth/model/user.dart';
 import 'package:capstone/modules/auth/provider/current_user_info.dart';
+import 'package:capstone/modules/comment/model/comment.dart';
 import 'package:capstone/modules/comment/viewmodel/comment_viewmodel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -20,13 +23,48 @@ class CommentWidget extends StatelessWidget {
           itemCount: comments.length,
           itemBuilder: (_, index) {
             final comment = comments[index];
-            return ListTile(
-              leading: Icon(Icons.person),
-              title: Text(comment.body ?? 'asu'),
-            );
+            return _commentItem(comment);
           },
         );
       },
+    );
+  }
+
+  Widget _commentItem(UserComment comment) {
+    final user = comment.user;
+    if (user == null) return Container();
+    return FutureBuilder<DocumentSnapshot>(
+      future: user.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final data = snapshot.data?.data() as Map<String, dynamic>?;
+          if (data == null) return Container();
+          final userInfo = User.fromMap(data);
+          return ListTile(
+            leading: _avatar(userInfo),
+            title: Text(userInfo.name ?? ''),
+            subtitle: Text(comment.body ?? ''),
+          );
+        }
+        return Container();
+      },
+    );
+  }
+
+  Widget _avatar(User userInfo) {
+    final avatarUrl = userInfo.avatarUrl;
+    if (avatarUrl == null) {
+      return const Icon(
+        Icons.person,
+        size: 32,
+      );
+    }
+    return CachedNetworkImage(
+      width: 32,
+      height: 32,
+      fit: BoxFit.cover,
+      imageUrl: avatarUrl,
+      errorWidget: (context, url, error) => const Icon(Icons.error),
     );
   }
 }
