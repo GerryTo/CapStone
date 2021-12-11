2f7301697773bc8012a2f0f3f663d2e2dc4a19b7
q <mboh.dong@gmail.com>
2021-12-12 00:08:08 +0700

    fix bug missing favorite

--- lib/modules/favorite/viewmodels/favorite_viewmodel.dart
+++ lib/modules/favorite/viewmodels/favorite_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:capstone/modules/feeds/model/feed.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -6,28 +8,33 @@ class FavoriteViewModel extends ChangeNotifier {
   List<Feed> favorites = [];
   final DocumentReference? userRef;
   final favoriteCollection = FirebaseFirestore.instance.collection('favorites');
+  static const tag = 'FavoriteViewModel';
 
   FavoriteViewModel(this.userRef) {
     getFavorites();
   }
 
   Future<void> getFavorites() async {
+    log('Get Favorite', name: tag);
     favorites.clear();
     try {
       final snapshot =
           await favoriteCollection.where("user", isEqualTo: userRef).get();
       final docs = snapshot.docs;
+      log(docs.toString(), name: tag);
       for (final doc in docs) {
+        log('fetch individual project', name: tag);
         final DocumentReference projectRef = doc.data()['project'];
         final projectSnapshot = await projectRef.get();
         final projectData = projectSnapshot.data() as Map<String, dynamic>?;
-        if (projectData == null) break;
+        if (projectData == null) continue;
         final project =
             Feed.fromMap(projectData).copyWith(ref: projectSnapshot.reference);
+        log(project.toString(), name: tag);
         favorites.add(project);
       }
     } on Exception catch (e) {
-      // TODO
+      log(e.toString(), name: tag);
     } finally {
       notifyListeners();
     }
