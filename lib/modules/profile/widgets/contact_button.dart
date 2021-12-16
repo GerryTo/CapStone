bf24f51a5ff44a148e4fbec9465d5521cd1a046d
q <mboh.dong@gmail.com>
2021-12-16 12:30:29 +0700

    cek user sudah login saat mengklik tombol kontak

--- lib/modules/profile/widgets/contact_button.dart
+++ lib/modules/profile/widgets/contact_button.dart
@@ -1,4 +1,6 @@
+import 'package:capstone/modules/auth/provider/current_user_info.dart';
 import 'package:capstone/modules/profile/viewmodel/profile_viewmodel.dart';
+import 'package:capstone/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -13,13 +15,23 @@ class ContactButton extends StatelessWidget {
     return Expanded(
       child: ElevatedButton(
         onPressed: () {
-          final Uri contact = Uri(
-            scheme: 'tel',
-            path: Provider.of<ProfileViewModel>(context, listen: false)
-                .user
-                ?.phone,
-          );
-          launch(contact.toString());
+          final uid = Provider.of<CurrentUserInfo>(
+            context,
+            listen: false,
+          ).id;
+
+          final phone = Provider.of<ProfileViewModel>(
+            context,
+            listen: false,
+          ).user?.phone;
+
+          if (uid == null) {
+            Routes.router.navigateTo(context, Routes.login);
+          } else if (phone == null) {
+            return;
+          } else {
+            _launchPhone(phone);
+          }
         },
         child: Wrap(
           children: const [
@@ -38,5 +50,13 @@ class ContactButton extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  _launchPhone(String phoneNumber) {
+    final Uri contact = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    launch(contact.toString());
   }
 }
